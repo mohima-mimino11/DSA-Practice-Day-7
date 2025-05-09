c6b6803a0939ea9e0d46b13e9fec9f5a7a14b166
mohima-mimino11 <mohimahoque22@gmail.com>
2025-05-09 21:34:14 +0600

    more pattern practice added

--- shape4.c
+++ shape4.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 int main(){
-  int n;
+  int n; 
   scanf("%d", &n);
-  int star = n;
+  int star = n+2;
   int space = 0;
-  for(int i = 1; i <= n; i++){
-    for(int k=1; k <= space; k++){
+  for(int i=1; i <= n; i++){
+    for(int j=1; j <= space; j++){
       printf(" ");
     }
-    for(int j=1; j <= star; j++){
+    for(int k=1; k <= star; k++){
       printf("*");
     }
     printf("\n");
-    star--;
+    star -= 2;
     space++;
+
   }
   return 0;
 }
